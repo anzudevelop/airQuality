713e307c2edd656c1ae3f0ce4fdd10a143bdb4ce
alexkosogor <alexk-00@mail.ru>
2022-06-20 17:52:38 +0700

    chartsFullRefactoringWithAdaptive

--- src/components/Charts/Chart/Chart.jsx
+++ src/components/Charts/Chart/Chart.jsx
@@ -1,4 +1,5 @@
 import s from './Chart.module.css'
+import React from 'react';
 import {
     Chart as ChartJS,
     CategoryScale,
@@ -8,62 +9,114 @@ import {
     Title,
     Tooltip,
     Legend,
-} from "chart.js"
-
-import { Line } from "react-chartjs-2";
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
 
 let Chart = (props) => {
     const dataPoints = props.values
     const times = props.dates
 
-    const data = {
+    ChartJS.register(
+        CategoryScale,
+        LinearScale,
+        PointElement,
+        LineElement,
+        Title,
+        Tooltip,
+        Legend
+    );
+
+     const data = {
         labels: times,
-        datasets: [{
-            label: props.label,
-            fill: true,
-            backgroundColor: 'rgb(196,206,218)',
-            borderColor: 'rgb(196,206,218)',
-            data: dataPoints,
-        }]
+        datasets: [
+            {
+                label: props.label,
+                data: dataPoints,
+                fill: true,
+                borderColor: 'rgb(255,255,255)',
+                backgroundColor: 'rgba(255,255,255,1)',
+                tension: 0.2,
+            },
+        ]
     };
 
-    const config = {
-        type: 'line',
-        data: data,
-        options: {
-            responsive: true,
-            plugins: {
-                title: {
-                    display: true,
-                    textDecorationColor: 'rgb(196,206,218)',
-                },
+    let titleFontSize = '20vw'
+    let titleColor = 'rgb(227,227,227)'
+    let xScaleFontSize = '15vw'
+    let xLabelColor = 'rgb(227,227,227)'
+    let yScaleLabelFontSize = '15vh'
+    let yScaleFontSize = '12vh'
+    let yLabelColor = 'rgb(227,227,227)'
+    let gridColor = 'rgb(87,87,87)'
+
+    const options = {
+        responsive: true,
+        maintainAspectRatio: false,
+        interaction: {
+            intersect: false,
+            axis: 'x'
+        },
+        plugins: {
+            legend: {
+                display: false,
+                position: 'top',
             },
-            interaction: {
-                mode: 'index',
-                intersect: true
-            },
-            scales: {
-                x: {
-                    display: true,
-                    title: {
-                        display: true,
-                        text: 'Month',
-                    }
-                },
-                y: {
-                    display: true,
-                    title: {
-                        display: true,
-                        text: 'Value'
-                    }
+            title: {
+                display: true,
+                text: props.title,
+                color: titleColor,
+                font: {
+                    size: titleFontSize,
                 }
             },
         },
+        scales: {
+            y: {
+                title: {
+                    display: true,
+                    align: 'center',
+                    text: props.label,
+                    color: yLabelColor,
+                    font: {
+                        size: yScaleLabelFontSize,
+                    },
+                },
+                grid: {
+                    color: gridColor,
+                },
+                ticks: {
+                    color: yLabelColor,
+                    font: {
+                        size: yScaleFontSize,
+                    }
+                }
+            },
+            x: {
+                title: {
+                    display: true,
+                    align: 'center',
+                    text: 'Время, с',
+                    color: xLabelColor,
+                    font: {
+                        size: xScaleFontSize,
+                    },
+                },
+                grid: {
+                    color: gridColor,
+                },
+                ticks: {
+                    color: xLabelColor,
+                    font: {
+                        size: xScaleFontSize,
+                    }
+                }
+            }
+        }
     };
 
     return (
         <div className={s.chartBody}>
-            <Line data={data} options={config}/>
+            <Line options={options} data={data}/>
         </div>
     );
 }
--- src/components/Charts/Charts.jsx
+++ src/components/Charts/Charts.jsx
@@ -32,61 +32,73 @@ let Charts = (props) => {
             <div className={s.chartsArea}>
 
                 <Chart
+                    title={'MHZ19B'}
                     label={'Концентрация CO2, ppm'}
                     values={sortFunction(props.sensorsData.mhz19b, 'co2_concentration')}
                     dates={sortFunction(props.sensorsData.mhz19b, 'date_and_time_of_measurement')}
                 />
                 <Chart
+                    title={'TGS2611'}
                     label={'Концентрация CH4, ppm'}
                     values={sortFunction(props.sensorsData.tgs2611, 'ch4_concentration')}
                     dates={sortFunction(props.sensorsData.tgs2611, 'date_and_time_of_measurement')}
                 />
                 <Chart
+                    title={'SDS011'}
                     label={'Количество взвешенных частиц pm2,5, мг/м^3'}
                     values={sortFunction(props.sensorsData.sds011, 'pm_2_5_concentration')}
                     dates={sortFunction(props.sensorsData.sds011, 'date_and_time_of_measurement')}
                 />
                 <Chart
+                    title={'SDS011'}
                     label={'Количество взвешенных частиц pm10, мг/м^3'}
                     values={sortFunction(props.sensorsData.sds011, 'pm_10_concentration')}
                     dates={sortFunction(props.sensorsData.sds011, 'date_and_time_of_measurement')}
                 />
                 <Chart
+                    title={'BMP280'}
                     label={'Атмосферное давление, мм. рт. ст.'}
                     values={sortFunction(props.sensorsData.bmp280, 'pressure')}
                     dates={sortFunction(props.sensorsData.bmp280, 'date_and_time_of_measurement')}
                 />
                 <Chart
+                    title={'BMP280'}
                     label={'Температура, °C'}
                     values={sortFunction(props.sensorsData.bmp280, 'temperature')}
                     dates={sortFunction(props.sensorsData.bmp280, 'date_and_time_of_measurement')}
                 />
                 <Chart
+                    title={'BMP280'}
                     label={'Высота над уровнем моря, м'}
                     values={sortFunction(props.sensorsData.bmp280, 'attitude')}
                     dates={sortFunction(props.sensorsData.bmp280, 'date_and_time_of_measurement')}
                 />
                 <Chart
+                    title={'SHT30'}
                     label={'Относительная влажность, %'}
                     values={sortFunction(props.sensorsData.sht30, 'humidity')}
                     dates={sortFunction(props.sensorsData.sht30, 'date_and_time_of_measurement')}
                 />
                 <Chart
+                    title={'SHT30'}
                     label={'Температура, °C'}
                     values={sortFunction(props.sensorsData.sht30, 'temperature')}
                     dates={sortFunction(props.sensorsData.sht30, 'date_and_time_of_measurement')}
                 />
                 <Chart
+                    title={'DS18B20'}
                     label={'Температура с первого датчика, °C'}
                     values={sortFunction(props.sensorsData.ds18b20, 'temperature_1')}
                     dates={sortFunction(props.sensorsData.ds18b20, 'date_and_time_of_measurement')}
                 />
                 <Chart
+                    title={'DS18B20'}
                     label={'Температура со второго датчика, °C'}
                     values={sortFunction(props.sensorsData.ds18b20, 'temperature_2')}
                     dates={sortFunction(props.sensorsData.ds18b20, 'date_and_time_of_measurement')}
                 />
                 <Chart
+                    title={'DS18B20'}
                     label={'Температура с третьего датчика, °C'}
                     values={sortFunction(props.sensorsData.ds18b20, 'temperature_3')}
                     dates={sortFunction(props.sensorsData.ds18b20, 'date_and_time_of_measurement')}
